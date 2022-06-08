9ce0a906787ae198e1f6c2c72cd38015715916d0
me-cafebabe <me_cafebabe@hotmail.com>
2022-06-09 05:44:41 +0800

    Revert "mi8937: Use common cryptfshw if exists"
    It no longer exist
    
    This reverts commit cfb579b3dc4d61eb01881150f173864f821637a1.

--- device.mk
+++ device.mk
@@ -21,15 +21,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libcryptfs_hw_intermediates/libcryptfs_hw.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libcryptfs_hw.so
 
-ifeq ($(wildcard vendor/qcom/opensource/cryptfs_hw/Android.bp),)
-PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)/cryptfs_hw
-else
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/vendor/cryptfshw,$(TARGET_COPY_OUT_RECOVERY)/root) \
-    $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/vendor.qti.hardware.cryptfshw@1.0_intermediates/vendor.qti.hardware.cryptfshw@1.0.so:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib64/hw/vendor.qti.hardware.cryptfshw@1.0.so
-endif
-
 # Keymaster
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.keystore=msm8937
