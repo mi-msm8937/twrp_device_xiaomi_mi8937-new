828f390dad834dae02dc4d7a276eda740f69fe59
me-cafebabe <me_cafebabe@hotmail.com>
2022-07-16 23:52:22 +0800

    mi8937: Use oem partition as metadata

--- recovery/root/system/bin/protect-partitions.sh
+++ recovery/root/system/bin/protect-partitions.sh
@@ -2,7 +2,7 @@
 
 # Manually Set Variables
 BOOTDEVICE="$(getprop ro.boot.bootdevice)"
-UNPROTECTED_PARTITIONS_REGEX="^boot$|^recovery$|^cache$|^userdata$|^system$|^cust$|^misc$|^persist$|^splash$|^fs.$|^modemst.$"
+UNPROTECTED_PARTITIONS_REGEX="^boot$|^recovery$|^cache$|^userdata$|^system$|^cust$|^oem$|^misc$|^persist$|^splash$|^fs.$|^modemst.$"
 
 # Generated variables
 PROTECTED_PARTITIONS="$(ls /dev/block/platform/soc/${BOOTDEVICE}/by-name/|eval grep -vE \"${UNPROTECTED_PARTITIONS_REGEX}\")"
