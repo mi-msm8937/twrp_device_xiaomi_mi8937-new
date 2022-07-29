Honor-MSM8937/twrp_device_honor_msm8937-new

mi8937: blksetro: Rename module to avoid build conflict

device.mk
@@ -40,6 +40,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Utils
 PRODUCT_PACKAGES += \
-    blksetro
+    blksetro.mi8937
 
 $(call inherit-product-if-exists, vendor/extras/product.mk)

recovery/root/system/bin/protect-partitions.sh
@@ -10,7 +10,7 @@ PROTECTED_PARTITIONS="$(ls /dev/block/platform/soc/${BOOTDEVICE}/by-name/|eval g
 # Make protected partitions read-only
 log "Protected partitions: " $PROTECTED_PARTITIONS
 for p in $PROTECTED_PARTITIONS; do
-    blksetro /dev/block/platform/soc/${BOOTDEVICE}/by-name/${p} || true
+    blksetro.mi8937 /dev/block/platform/soc/${BOOTDEVICE}/by-name/${p} || true
 done
 
 exit 0

mi-msm8937/twrp_device_xiaomi_mi8937-new

device.mk
@@ -40,6 +40,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Utils
 PRODUCT_PACKAGES += \
-    blkroset
+    blkroset.mi8937
 
 $(call inherit-product-if-exists, vendor/extras/product.mk)

recovery/root/system/bin/protect-partitions.sh
@@ -10,7 +10,7 @@ PROTECTED_PARTITIONS="$(ls /dev/block/platform/soc/${BOOTDEVICE}/by-name/|eval g
 # Make protected partitions read-only
 log "Protected partitions: " $PROTECTED_PARTITIONS
 for p in $PROTECTED_PARTITIONS; do
-    blkroset /dev/block/platform/soc/${BOOTDEVICE}/by-name/${p} || true
+    blkroset.mi8937 /dev/block/platform/soc/${BOOTDEVICE}/by-name/${p} || true
 done
 
 exit 0