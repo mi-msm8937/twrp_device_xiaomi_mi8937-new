Honor-MSM8937/twrp_device_honor_msm8937-new

mi8937: Use my own BLKROSET_PERMANENT ioctl to set block device to read-only

recovery/root/system/bin/protect-partitions.sh
@@ -10,7 +10,7 @@ PROTECTED_PARTITIONS="$(ls /dev/block/platform/soc/${BOOTDEVICE}/by-name/|eval g
 # Make protected partitions read-only
 log "Protected partitions: " $PROTECTED_PARTITIONS
 for p in $PROTECTED_PARTITIONS; do
-    blockdev --setro /dev/block/platform/soc/${BOOTDEVICE}/by-name/${p} || true
+    blksetro /dev/block/platform/soc/${BOOTDEVICE}/by-name/${p} || true
 done
 
 exit 0

mi-msm8937/twrp_device_xiaomi_mi8937-new

recovery/root/system/bin/protect-partitions.sh
@@ -10,7 +10,7 @@ PROTECTED_PARTITIONS="$(ls /dev/block/platform/soc/${BOOTDEVICE}/by-name/|eval g
 # Make protected partitions read-only
 log "Protected partitions: " $PROTECTED_PARTITIONS
 for p in $PROTECTED_PARTITIONS; do
-    blockdev --setro /dev/block/platform/soc/${BOOTDEVICE}/by-name/${p} || true
+    blkroset /dev/block/platform/soc/${BOOTDEVICE}/by-name/${p} || true
 done
 
 exit 0